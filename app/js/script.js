884ed45ab38553deee5fc4923129c0507c3d97ca
Daniil Ruzin <jeffgoldtv7@gmail.com>
2020-10-15 19:51:02 +0300

    initial

---
+++ app/js/script.js
@@ -0,0 +1,13 @@
+
+document.addEventListener('click', function(e){
+    const btn = e.target.closest('.header__burger');
+    
+    if(!btn) return false
+
+    const   header = btn.closest('.header'),
+            header__menuWrapper = header.querySelector('.header__menu-wrapper');
+
+    header__menuWrapper.classList.toggle('header__menu-wrapper_show')
+
+
+});
